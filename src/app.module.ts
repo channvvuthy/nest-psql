f1b4ecadf4df771b3c648380967cee4fb231edbb
vuthy <vuthy104.infinity@gmail.com>
2023-10-17 08:19:58 +0700

    Clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import dbConfig from './config/db';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dbConfig)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
